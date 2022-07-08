87c77677af513c949454347103a07029fff32a73
ubukawa <ubukawa-t96dq@mlit.go.jp>
2022-07-08 01:37:16 -0400

    rev

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const config = require('config')
+const fs = require('fs')
 const Queue = require('better-queue')
 const { spawn } = require('child_process')
 const Parser = require('json-text-sequence').parser
+const tilebelt = require('@mapbox/tilebelt')
 
 const srcdb = config.get('srcdb')
 const ogr2ogrPath = config.get('ogr2ogrPath')
 const tippecanoePath = config.get('tippecanoePath')
 const minzoom = config.get('minzoom')
 const maxzoom = config.get('maxzoom')
+const mbtilesDir = config.get('mbtilesDir')
 
 let keyInProgress = []
 let idle = true
@@ -17,7 +20,7 @@ const isIdle = () => {
 }
 
 
-const dumpAndModify = async(downstream, key) => {
+const dumpAndModify = async(downstream, key, tile) => {
     return new Promise((resolve, reject) =>{
         //from here
         const parser = new Parser()
@@ -35,17 +38,20 @@ const dumpAndModify = async(downstream, key) => {
                 resolve() //check
             }
             )
+        const [z, x, y] = tile
+        const bbox = tilebelt.tileToBBOX([x, y, z])     
         var ogr2ogr = spawn(ogr2ogrPath, [
             '-f', 'GeoJSONSeq',
             '-lco', 'RS=YES',
             '/vsistdout/',
         //    '-clipdst', 0, 52.4827, 5.625, 55.76573,
-        //    srcdb.url
-            `small-data/${key}.geojson`
+            '-clipdst', bbox[0], bbox[1], bbox[2], bbox[3],
+            srcdb.url
+            //`small-data/${key}.geojson`
         ])
         ogr2ogr.on('exit', () => {
             let nowTime = new Date()
-            //console.log(`${key}: GDAL reading ends at ${nowTime}:\n`)
+            console.log(`${key}: GDAL reading ends at ${nowTime}:\n`)
         })
 
         ogr2ogr.stdout.pipe(parser)
@@ -64,21 +70,28 @@ const sleep = (wait) => {
 const queue = new Queue(async (t, cb) => {
     const startTime = new Date()
     const key = t.key
-    const [z, x, y] = t.tile
-    const bbox = [6,32,20] // will be edited
+    const tile = t.tile
+    const [z, x, y] = tile
+    const tmpPath = `${mbtilesDir}/part-${z}-${x}-${y}.mbtiles`
+    const dstPath = `${mbtilesDir}/${z}-${x}-${y}.mbtiles`
 
     keyInProgress.push(key)
     console.log(`[${keyInProgress}] in progress`)
     await sleep(1000)
 // conversion
     const tippecanoe = spawn(tippecanoePath, [
-        `--output=${key}.mbtiles`,
+        `--output=${tmpPath}`,
+        '--no-feature-limit',
+        '--no-tile-size-limit',
         '--force',
+        '--simplification=2',
+        //boundinb box or tile will be added
         '--quiet',
         `--minimum-zoom=${minzoom}`,
         `--maximum-zoom=${maxzoom}`
     ], { stdio: ['pipe', 'inherit', 'inherit']})
        .on('exit', () => {
+            fs.renameSync(tmpPath, dstPath)
             const endTime = new Date()
             console.log(`${key}: Tippecanoe ${startTime.toISOString()} --> ${endTime.toISOString()} (^o^)/`)
             keyInProgress = keyInProgress.filter((v) => !(v === key))
@@ -88,22 +101,25 @@ const queue = new Queue(async (t, cb) => {
         await sleep(5000)
     }
     try {
-        await dumpAndModify(tippecanoe.stdin, key)
+        await dumpAndModify(tippecanoe.stdin, key, tile)
     } catch(e) {
         cb(true)
     }
     tippecanoe.stdin.end()
 },{
-    concurrent: 2,
-    maxRetries: 3,
-    retryDelay: 5000
+    concurrent: config.get('concurrent'),
+    maxRetries: config.get('maxRetries'),
+    retryDelay: config.get('retryDelay')
 })
 
 
 const queueTasks = () => {
-    for (let key of ['bndl1', 'bndl2', 'bndl3', 'bndl4', 'bndl5', 'bndl6']){
-        queue.push({
-            key: key
+    //for (let key of ['bndl1', 'bndl2', 'bndl3', 'bndl4', 'bndl5', 'bndl6']){
+    for (let tile of srcdb.tiles){
+        const key = `${tile[0]}-${tile[1]}-${tile[2]}`
+    queue.push({
+            key: key,
+            tile: tile
         })
     }
 }
